a9b43575c45fd66ee72b3ee9a9863164fca01061
Reinaldo Ignatius Wijaya <reinaldo@agate.id>
2017-11-11 23:28:00 +0700

    Finished header

--- nav.php
+++ nav.php
@@ -3,39 +3,42 @@
 <head>
 	<title>DEAR - Poliklinik</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" href="css\nav.css"
 </head>
 <body>
-	<div class='header'>
-		<img class='logo' src='IMK/resources/images/logo.png'>
-		<div class='title'>Poliklinik DEAR</div>
-		<div class='contact'>
-			<p>
+	<div class='container-fluid'>
+		<div class='row vertical-align'>
+			<div class='col-md-2 logo'>
+				<img class='center-block' src='resources\images\logo.png'>
+			</div>
+			<div class='col-md-7'>
+				<h1>DEAR - Poliklinik</h1>
+			</div>
+			<div class='col-md-3'>
 				<b>Alamat</b> Jl. ABCD No.123 - Bandung, Jawa Barat<br>
 				<b>Telepon</b> 0123 - 123456<br>
 				<b>Email</b> poliklinikdear@gmail.com<br>
-			</p>
+			</div>
 		</div>
 	</div>
 
-	<div class='container'>
-		<nav class="nav flex-column">
-			<ul class="nav nav-tabs">
-			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melihat <span class='red'>JADWAL</span> poliklinik</a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melakukan <span class='red'>KONSTULTASI</span></a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melakukan <span class='red'></span></a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melihat <span class='red'>KETERSEDIAAN OBAT</span></a>
-			  </li>
-			</ul>
-		</nav>
+	<hr>
 
-		<button class='ambulance'>Pesan Ambulance</button>
+	<div class="container-fluid">
+		<div class="row">
+		  <div class="col-md-3">
+		    <ul class="nav nav-pills nav-stacked">
+					<li>jedan</li>
+		    	<li>dva</li>
+  		 	 	<li>tri</li>
+					<li>cetriri</li>
+					<li>pet</li>
+				</ul>
+	    </div>
+	  	<div class="col-md-9">
+		    Main content goes here
+		  </div>
+		</div>
 	</div>
 
 </body>
