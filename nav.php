9e7ba5b9564c749866850e19bc9eeb265098e36e
Erick Wijaya <wijaya.erick52@gmail.com>
2017-11-11 22:19:58 +0700

    header

--- nav.php
+++ nav.php
@@ -3,39 +3,55 @@
 <head>
 	<title>DEAR - Poliklinik</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-	<div class='header'>
-		<img class='logo' src='IMK/resources/images/logo.png'>
-		<div class='title'>Poliklinik DEAR</div>
-		<div class='contact'>
-			<p>
-				<b>Alamat</b> Jl. ABCD No.123 - Bandung, Jawa Barat<br>
-				<b>Telepon</b> 0123 - 123456<br>
-				<b>Email</b> poliklinikdear@gmail.com<br>
-			</p>
+	
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2">
+					<img src="resources/images/logo.png" class="img-fluid">
+				</div>
+				<div class="col-lg-4 valign-center">
+					<div class="title">Poliklinik DEAR</div>
+				</div>
+				<div class="col-lg-6 valign-center">
+					<p>
+						<b>Alamat</b> Jl. ABCD No.123 - Bandung, Jawa Barat<br>
+						<b>Telepon</b> 0123 - 123456<br>
+						<b>Email</b> poliklinikdear@gmail.com<br>
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
+	<br>
 
-	<div class='container'>
+	<button type="button" class="btn btn-primary bmd-btn-fab">
+			<i class="material-icons">grade</i>
+		</button>
+
+
+	<div class="container">
 		<nav class="nav flex-column">
 			<ul class="nav nav-tabs">
 			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melihat <span class='red'>JADWAL</span> poliklinik</a>
+			    <a class="nav-link" href="#">Saya ingin melihat <span class="red">JADWAL</span> poliklinik</a>
 			  </li>
 			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melakukan <span class='red'>KONSTULTASI</span></a>
+			    <a class="nav-link" href="#">Saya ingin melakukan <span class="red">KONSTULTASI</span></a>
 			  </li>
 			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melakukan <span class='red'></span></a>
+			    <a class="nav-link" href="#">Saya ingin melakukan <span class="red"></span></a>
 			  </li>
 			  <li class="nav-item">
-			    <a class="nav-link active" href="#">Saya ingin melihat <span class='red'>KETERSEDIAAN OBAT</span></a>
+			    <a class="nav-link" href="#">Saya ingin melihat <span class="red">KETERSEDIAAN OBAT</span></a>
 			  </li>
 			</ul>
 		</nav>
 
-		<button class='ambulance'>Pesan Ambulance</button>
+		<button class="ambulance">Pesan Ambulance</button>
 	</div>
 
 </body>
