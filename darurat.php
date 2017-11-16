39919019d579cdd0c3e3a615aa6522891ea927af
Erick Wijaya <wijaya.erick52@gmail.com>
2017-11-16 23:33:34 +0700

    maps

---
+++ darurat.php
@@ -0,0 +1,16 @@
+<?php
+    include 'nav.html'  
+?>
+
+<link rel="stylesheet" href="css\darurat.css">
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChhx7mXbGtUxphYxzoWeRQBjm_wriLAQI&libraries=places&callback=initAutocomplete" 
+async defer></script>
+<script type="text/javascript" src="script/darurat.js"></script>
+
+<input id="pac-input" class="controls" type="text" placeholder="Search Box">
+<div id="map"></div>
+
+
+
+</body>
+</html>
