ae12293600a9ffc6f4b63d85da7aaf05891b8afe
annisanurulazhar <annisanurulazhar@yahoo.com>
2017-11-17 13:13:15 +0700

    add obat.php

---
+++ obat.php
@@ -0,0 +1,35 @@
+<?php
+	include 'nav.html' 
+?>
+
+<link rel="stylesheet" href="css\Jadwal.css">
+
+<div>
+	<h1 align="center">Stok Obat</h1>
+	<table>
+  <tr>
+    <td>No</td>
+    <td>Nama Obat</td>
+    <td>Satuan</td>
+    <td>Stok Obat</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Anisol</td>
+    <td>btl/100ml</td>
+    <td>16</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Antalgin inj</td>
+    <td>box/30 amp</td>
+    <td>277</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Antalgin tab 500mg</td>
+    <td>box/100 tab</td>
+    <td>1528</td>
+  </tr>
+</table>
+</div>
