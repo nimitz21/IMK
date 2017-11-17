5aaa78dd6794b1428ebb5324102ba77b7a810997
annisanurulazhar <annisanurulazhar@yahoo.com>
2017-11-17 12:39:39 +0700

    add jadwal.php

---
+++ jadwal.php
@@ -0,0 +1,68 @@
+<?php
+	include 'nav.html' 
+?>
+
+<link rel="stylesheet" href="css\Jadwal.css">
+
+<div>
+	<h1 align="center">Jadwal Poliklinik</h1>
+	<table>
+  <tr>
+    <td rowspan="2">Jam</td>
+    <td colspan="6">Hari</td>
+  </tr>
+  <tr>
+    <td>Senin</td>
+    <td>Selasa</td>
+    <td>Rabu</td>
+    <td>Kamis</td>
+    <td>Jumat</td>
+    <td>Sabtu</td>
+  </tr>
+  <tr>
+    <td>07.00-08.00</td>
+    <td>dr. A</td>
+    <td>dr. B</td>
+    <td>dr. C</td>
+    <td>dr. D</td>
+    <td>dr. E</td>
+    <td>Dokter Jaga</td>
+  </tr>
+  <tr>
+    <td>08.00-09.00</td>
+    <td>dr. A</td>
+    <td>dr. B</td>
+    <td>dr. C</td>
+    <td>dr. D</td>
+    <td>dr. E</td>
+    <td>Dokter Jaga</td>
+  </tr>
+  <tr>
+    <td>09.00-10.00</td>
+    <td>dr. A</td>
+    <td>dr. B</td>
+    <td>dr. C</td>
+    <td>dr. D</td>
+    <td>dr. E</td>
+    <td>Dokter Jaga</td>
+  </tr>
+  <tr>
+    <td>10.00-11.00</td>
+    <td>dr. A</td>
+    <td>dr. B</td>
+    <td>dr. C</td>
+    <td>dr. D</td>
+    <td>dr. E</td>
+    <td>Dokter Jaga</td>
+  </tr>
+  <tr>
+    <td>12.00-13.00</td>
+    <td>dr. A</td>
+    <td>dr. B</td>
+    <td>dr. C</td>
+    <td>dr. D</td>
+    <td>dr. E</td>
+    <td>Dokter Jaga</td>
+  </tr>
+</table>
+</div>
