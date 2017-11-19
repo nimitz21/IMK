ee18abc5c2a250c5d5fd9899291032e508fe7022
annisanurulazhar <annisanurulazhar@yahoo.com>
2017-11-19 07:11:26 +0700

    Change layout obat.php and jadwal.php

--- jadwal.php
+++ jadwal.php
@@ -1,68 +1,327 @@
 <?php
-	include 'nav.html' 
+	include 'nav.html'
 ?>
 
 <link rel="stylesheet" href="css\Jadwal.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<div>
-	<h1 align="center">Jadwal Poliklinik</h1>
-	<table>
-  <tr>
-    <td rowspan="2">Jam</td>
-    <td colspan="6">Hari</td>
-  </tr>
-  <tr>
-    <td>Senin</td>
-    <td>Selasa</td>
-    <td>Rabu</td>
-    <td>Kamis</td>
-    <td>Jumat</td>
-    <td>Sabtu</td>
-  </tr>
-  <tr>
-    <td>07.00-08.00</td>
-    <td>dr. A</td>
-    <td>dr. B</td>
-    <td>dr. C</td>
-    <td>dr. D</td>
-    <td>dr. E</td>
-    <td>Dokter Jaga</td>
-  </tr>
-  <tr>
-    <td>08.00-09.00</td>
-    <td>dr. A</td>
-    <td>dr. B</td>
-    <td>dr. C</td>
-    <td>dr. D</td>
-    <td>dr. E</td>
-    <td>Dokter Jaga</td>
-  </tr>
-  <tr>
-    <td>09.00-10.00</td>
-    <td>dr. A</td>
-    <td>dr. B</td>
-    <td>dr. C</td>
-    <td>dr. D</td>
-    <td>dr. E</td>
-    <td>Dokter Jaga</td>
-  </tr>
-  <tr>
-    <td>10.00-11.00</td>
-    <td>dr. A</td>
-    <td>dr. B</td>
-    <td>dr. C</td>
-    <td>dr. D</td>
-    <td>dr. E</td>
-    <td>Dokter Jaga</td>
-  </tr>
-  <tr>
-    <td>12.00-13.00</td>
-    <td>dr. A</td>
-    <td>dr. B</td>
-    <td>dr. C</td>
-    <td>dr. D</td>
-    <td>dr. E</td>
-    <td>Dokter Jaga</td>
-  </tr>
-</table>
+<div class="container">
+	<div class="row">
+		<h1 align="center">Jadwal Poliklinik</h1>
+	</div>
+	<div class="row">
+		<div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+					<div class="row">
+						<div class="col-md-9">
+								<p>Gigi</p>
+						</div>
+						<div class="col-md-3" align="right">
+							  <button data-toggle="collapse" data-parent="#accordion" href="#collapse1">Lihat jadwal</button>
+						</div>
+					</div>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse">
+        <div class="panel-body">
+					<table>
+				  <tr>
+				    <td rowspan="2">Jam</td>
+				    <td colspan="6">Hari</td>
+				  </tr>
+				  <tr>
+				    <td>Senin</td>
+				    <td>Selasa</td>
+				    <td>Rabu</td>
+				    <td>Kamis</td>
+				    <td>Jumat</td>
+				    <td>Sabtu</td>
+				  </tr>
+				  <tr>
+				    <td>07.00-08.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>08.00-09.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>09.00-10.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>10.00-11.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>12.00-13.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				</table>
+				</div>
+      </div>
+    </div>
+		<div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+					<div class="row">
+						<div class="col-md-9">
+								<p>Mata</p>
+						</div>
+						<div class="col-md-3" align="right">
+							  <button data-toggle="collapse" data-parent="#accordion" href="#collapse2">Lihat jadwal</button>
+						</div>
+					</div>
+        </h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body">
+					<table>
+				  <tr>
+				    <td rowspan="2">Jam</td>
+				    <td colspan="6">Hari</td>
+				  </tr>
+				  <tr>
+				    <td>Senin</td>
+				    <td>Selasa</td>
+				    <td>Rabu</td>
+				    <td>Kamis</td>
+				    <td>Jumat</td>
+				    <td>Sabtu</td>
+				  </tr>
+				  <tr>
+				    <td>07.00-08.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>08.00-09.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>09.00-10.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>10.00-11.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>12.00-13.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				</table>
+				</div>
+      </div>
+    </div>
+		<div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+					<div class="row">
+						<div class="col-md-9">
+								<p>Radiologi</p>
+						</div>
+						<div class="col-md-3" align="right">
+							  <button data-toggle="collapse" data-parent="#accordion" href="#collapse3">Lihat jadwal</button>
+						</div>
+					</div>
+        </h4>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body">
+					<table>
+				  <tr>
+				    <td rowspan="2">Jam</td>
+				    <td colspan="6">Hari</td>
+				  </tr>
+				  <tr>
+				    <td>Senin</td>
+				    <td>Selasa</td>
+				    <td>Rabu</td>
+				    <td>Kamis</td>
+				    <td>Jumat</td>
+				    <td>Sabtu</td>
+				  </tr>
+				  <tr>
+				    <td>07.00-08.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>08.00-09.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>09.00-10.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>10.00-11.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>12.00-13.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				</table>
+				</div>
+      </div>
+    </div>
+		<div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+					<div class="row">
+						<div class="col-md-9">
+								<p>Umum</p>
+						</div>
+						<div class="col-md-3" align="right">
+							  <button data-toggle="collapse" data-parent="#accordion" href="#collapse4">Lihat jadwal</button>
+						</div>
+					</div>
+        </h4>
+      </div>
+      <div id="collapse4" class="panel-collapse collapse">
+        <div class="panel-body">
+					<table>
+				  <tr>
+				    <td rowspan="2">Jam</td>
+				    <td colspan="6">Hari</td>
+				  </tr>
+				  <tr>
+				    <td>Senin</td>
+				    <td>Selasa</td>
+				    <td>Rabu</td>
+				    <td>Kamis</td>
+				    <td>Jumat</td>
+				    <td>Sabtu</td>
+				  </tr>
+				  <tr>
+				    <td>07.00-08.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>08.00-09.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>09.00-10.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>10.00-11.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				  <tr>
+				    <td>12.00-13.00</td>
+				    <td>dr. A</td>
+				    <td>dr. B</td>
+				    <td>dr. C</td>
+				    <td>dr. D</td>
+				    <td>dr. E</td>
+				    <td>Dokter Jaga</td>
+				  </tr>
+				</table>
+				</div>
+      </div>
+    </div>
+  </div>
+	</div>
+</div>
 </div>
--- obat.php
+++ obat.php
@@ -1,35 +1,47 @@
 <?php
-	include 'nav.html' 
+	include 'nav.html'
 ?>
 
 <link rel="stylesheet" href="css\Jadwal.css">
-
-<div>
-	<h1 align="center">Stok Obat</h1>
-	<table>
-  <tr>
-    <td>No</td>
-    <td>Nama Obat</td>
-    <td>Satuan</td>
-    <td>Stok Obat</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>Anisol</td>
-    <td>btl/100ml</td>
-    <td>16</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td>Antalgin inj</td>
-    <td>box/30 amp</td>
-    <td>277</td>
-  </tr>
-  <tr>
-    <td>3</td>
-    <td>Antalgin tab 500mg</td>
-    <td>box/100 tab</td>
-    <td>1528</td>
-  </tr>
-</table>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<div class="container" style="margin: 20px">
+	<div class="row">
+		<div class="col-md-2">
+			<h5>Cari Berdasarkan</h5>
+		</div>
+		<div class="col-md-3">
+			<div class="dropdown">
+			    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Pilih salah satu
+			    <span class="caret"></span></button>
+			    <ul class="dropdown-menu">
+			      <li><a href="#">Nama</a></li>
+			      <li><a href="#">Kandungan</a></li>
+			    </ul>
+			  </div>
+		</div>
+		<div class="col-md-7">
+			<div class="form-group has-feedback">
+    		<input type="text" class="form-control" id="obat" placeholder="Cari obat"/>
+				 <i class="glyphicon glyphicon-search form-control-feedback"></i>
+  	 	</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="panel-group">
+		    <div class="panel panel-default">
+		      <div class="panel-heading">Panel Header</div>
+		      <div class="panel-body">Panel Content</div>
+		    </div>
+		    <div class="panel panel-default">
+		      <div class="panel-heading">Panel Header</div>
+		      <div class="panel-body">Panel Content</div>
+		    </div>
+		    <div class="panel panel-default">
+		      <div class="panel-heading">Panel Header</div>
+		      <div class="panel-body">Panel Content</div>
+		    </div>
+		  </div>
+	</div>
 </div>
